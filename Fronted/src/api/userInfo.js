6c027b553e4e8c1bb1fa1054bb04710a5944d6ba
Jefferson20204 <jefferson1999rosales@gmail.com>
2025-05-05 10:47:32 -0500

    a

--- Fronted/src/api/userInfo.js
+++ Fronted/src/api/userInfo.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { API_BASE_URL, getHeaders } from "./constant";
 
-// asda
 export const fetchUserDetails = async () => {
   const url = API_BASE_URL + "/api/user/profile";
   try {
